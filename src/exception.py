10c8d838205fbaa5740184cd5e8e277ec4890be8
Vibhore Jain <vjmj4005@gmail.com>
2023-12-31 19:37:18 +0530

    notebook

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,4 @@
-import logging
+from src.logger import logging
 import sys
 
 def error_msg_detail(error,error_detail:sys):
@@ -10,4 +10,12 @@ class CustomException(Exception):
     def __init__(self, error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message = error_msg_detail(error_message,error_detail=error_detail)
-    def __str__(self):return self.error_message
+    def __str__(self):
+        return self.error_message
+
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("divide error") 
+        raise CustomException(e,sys)
