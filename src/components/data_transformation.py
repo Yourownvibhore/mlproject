940e7682cf357f3d2188e74570b2e5eb49c742ec
Vibhore Jain <vjmj4005@gmail.com>
2024-01-02 16:31:51 +0530

    model trainer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -74,7 +74,7 @@ class DataTransformation:
 
             train_arr=np.c_[input_feature_train_arr,np.array(target_feature_train_df)]
 
-            test_arr=np.c_[input_feature_test_df,np.array(target_feature_test_df)]
+            test_arr=np.c_[input_feature_test_arr,np.array(target_feature_test_df)]
             logging.info("save the preprocessor object")
 
             save_object(
